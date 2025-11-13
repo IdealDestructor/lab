b48587b1593bf66211717da967f44408db31815b
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-11-13 17:19:24 -0300

    icons style

--- src/components/commons/socialIcons/socialIcons.jsx
+++ src/components/commons/socialIcons/socialIcons.jsx
@@ -12,7 +12,7 @@ const SocialIcons = () => {
         href="https://www.linkedin.com/in/lautaro-rodr%C3%ADguez-collins-40505624a/"
         target="_blank"
         rel="noopener noreferrer"
-        className="mr-3 text-zinc-400 hover:text-[#69c7c7]"
+        className="mr-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
         aria-label="Linkedin"
       >
         <FaLinkedinIn className="h-7 w-7" />
@@ -22,7 +22,7 @@ const SocialIcons = () => {
         href="https://github.com/Lautaro-R-collins"
         target="_blank"
         rel="noopener noreferrer"
-        className="mx-3 text-zinc-400 hover:text-[#69c7c7]"
+        className="mx-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
         aria-label="Github"
       >
         <FaGithub className="h-7 w-7" />
@@ -32,7 +32,7 @@ const SocialIcons = () => {
         href="https://x.com/lautarodevelops/"
         target="_blank"
         rel="noopener noreferrer"
-        className="mx-3 text-zinc-400 hover:text-[#69c7c7]"
+        className="mx-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
         aria-label="Twitter"
       >
         <FaXTwitter className="h-7 w-7" />
@@ -42,7 +42,7 @@ const SocialIcons = () => {
         href="https://www.instagram.com/lautarodevelops/"
         target="_blank"
         rel="noopener noreferrer"
-        className="mx-3 text-zinc-400 hover:text-[#69c7c7]"
+        className="mx-3 text-zinc-400 hover:text-[#69c7c7] drop-shadow-[1px_1px_0_#7836cf]"
         aria-label="Instagram"
       >
         <FaInstagram className="h-7 w-7" />
